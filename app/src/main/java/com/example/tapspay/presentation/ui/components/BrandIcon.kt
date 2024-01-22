4bdcf290ac07c3bb4408e75d7f4bf07d6a47184f
Saiful Islam <mdsaiful.islam@bjitgroup.com>
2024-01-22 17:19:58 +0600

    [Tap's Pay] - Add Icon

--- app/src/main/java/com/example/tapspay/presentation/ui/components/BrandIcon.kt
+++ app/src/main/java/com/example/tapspay/presentation/ui/components/BrandIcon.kt
@@ -2,10 +2,7 @@ package com.example.tapspay.presentation.ui.components
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Card
 import androidx.compose.runtime.Composable
@@ -20,7 +17,7 @@ import com.example.tapspay.utils.pxToDpWidth
 
 @Composable
 fun BrandIcon(
-    painter: Int
+    painter: Int,
 ) {
     val context = LocalContext.current
     Box(
